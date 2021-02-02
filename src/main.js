900f6a197a524579421e2681e6e68c1caa3a8523
mers85 <mers85@gmail.com>
2021-02-02 22:53:49 +0000

    generador de cartas usando js + css  aun le falta factorizar el codigo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,68 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  renderRandomCard(cardValues, suites);
+  console.log("Hello world!");
 };
+
+let cardValues = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let suites = ["heart", "diams", "clubs", "spades"];
+
+function sampleFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function createDivAndSetAttributeClass(strClass) {
+  let newDivWithClass = document.createElement("div");
+  newDivWithClass.setAttribute("class", strClass);
+  return newDivWithClass;
+}
+
+function addDivChild(divFather, divChild) {
+  divFather.appendChild(divChild);
+  return divFather;
+}
+
+function renderRandomCard(values, suites) {
+  let value = sampleFromArray(values);
+  let suite = sampleFromArray(suites);
+
+  let cardClass = "card" + " " + suite;
+
+  let divCard = createDivAndSetAttributeClass(cardClass);
+  let divIconHeader = createDivAndSetAttributeClass("card-icon-header");
+  let divIconFooter = createDivAndSetAttributeClass("card-icon-footer");
+
+  let divValue = createDivAndSetAttributeClass("card-value");
+  let cardValue = document.createTextNode(value);
+  addDivChild(divValue, cardValue);
+
+  divCard.appendChild(divIconHeader);
+  divCard.appendChild(divValue);
+  divCard.appendChild(divIconFooter);
+
+  let divWrapper = createDivAndSetAttributeClass("wrapper");
+  divWrapper.appendChild(divCard);
+
+  let divBody = document.getElementsByTagName("body")[0];
+  divBody.appendChild(divWrapper);
+}
